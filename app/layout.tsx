1cb6a51379f16bb76e02c84371b2b88d59e49001
Sangay Yoesel <sangayyoesel@gmail.com>
2026-04-21 16:09:13 +0600

    Add Google Analytics GA4

--- app/layout.tsx
+++ app/layout.tsx
@@ -58,6 +58,18 @@ export default function RootLayout({
             __html: `(function(c,l,a,r,i,t,y){c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);})(window,document,"clarity","script","wf3rv1xmng");`,
           }}
         />
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-6PC3WD41GG"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-6PC3WD41GG');
+          `}
+        </Script>
       </body>
     </html>
   )
